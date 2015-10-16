b1d36a9a69dcdc0928af184fd3c78f5f00c4dd9a
Thorworks <Joey Ferwerda>
2015-10-16 14:25:12 +0200

    Fixed the getf function and more work on fixing return types

--- OpenHMD-Java.c
+++ OpenHMD-Java.c
@@ -71,19 +71,177 @@ JNIEXPORT jint JNICALL Java_OpenHMD_ohmd_close_device(JNIEnv* env, jobject obj, 
 	return ohmd_close_device(device);
 }
 
-JNIEXPORT jfloat* JNICALL Java_OpenHMD_ohmd_device_getf(JNIEnv* env, jobject obj, jlong context, jint type);
+JNIEXPORT jfloatArray JNICALL Java_OpenHMD_ohmd_device_getf(JNIEnv* env, jobject obj, jlong context, jint type);
 {
 	ohmd_context* ctx = (ohmd_context*)context;
-	float* cout;
-	jfloat* jout;
+	
+	switch(type){
+	case OHMD_LEFT_EYE_GL_MODELVIEW_MATRIX: 
+	{
+		float[16] cout;
+		jfloatArray jout = (*env)->NewFloatArray(16);
+		
+		ohmd_device_getf(device, type, cout);
+		(*env)->SetFloatArrayRegion(env, jout, 0, 16, cout);
 
-	ohmd_device_getf(device, type, cout);
-	jout = (*env)->GetFloatArrayElements(env, cout, 0);
+		return jout;
+	}
+	case OHMD_RIGHT_EYE_GL_MODELVIEW_MATRIX: 
+	{
+		float[16] out;
+		jfloatArray jout = (*env)->NewFloatArray(16);
+		
+		ohmd_device_getf(device, type, cout);
+		(*env)->SetFloatArrayRegion(env, jout, 0, 16, cout);
 
-	return jout;
+		return jout;
+	}
+	case OHMD_LEFT_EYE_GL_PROJECTION_MATRIX:
+	{
+		float[16] out;
+		jfloatArray jout = (*env)->NewFloatArray(16);
+		
+		ohmd_device_getf(device, type, cout);
+		(*env)->SetFloatArrayRegion(env, jout, 0, 16, cout);
+
+		return jout;
+	}
+	case OHMD_RIGHT_EYE_GL_PROJECTION_MATRIX:
+	{
+		float[16] out;
+		jfloatArray jout = (*env)->NewFloatArray(16);
+		
+		ohmd_device_getf(device, type, cout);
+		(*env)->SetFloatArrayRegion(env, jout, 0, 16, cout);
+
+		return jout;
+	}
+	case OHMD_SCREEN_HORIZONTAL_SIZE:
+	{
+		float[1] out;
+		jfloatArray jout = (*env)->NewFloatArray(1);
+		
+		ohmd_device_getf(device, type, cout);
+		(*env)->SetFloatArrayRegion(env, jout, 0, 1, cout);
+
+		return jout;
+	}
+	case OHMD_SCREEN_VERTICAL_SIZE:
+	{
+		float[1] out;
+		jfloatArray jout = (*env)->NewFloatArray(1);
+		
+		ohmd_device_getf(device, type, cout);
+		(*env)->SetFloatArrayRegion(env, jout, 0, 1, cout);
+
+		return jout;
+	}
+
+	case OHMD_LENS_HORIZONTAL_SEPARATION:
+	{
+		float[1] out;
+		jfloatArray jout = (*env)->NewFloatArray(1);
+		
+		ohmd_device_getf(device, type, cout);
+		(*env)->SetFloatArrayRegion(env, jout, 0, 1, cout);
+
+		return jout;
+	}
+	case OHMD_LENS_VERTICAL_POSITION:
+	{
+		float[1] out;
+		jfloatArray jout = (*env)->NewFloatArray(1);
+		
+		ohmd_device_getf(device, type, cout);
+		(*env)->SetFloatArrayRegion(env, jout, 0, 1, cout);
+
+		return jout;
+	}
+
+	case OHMD_RIGHT_EYE_FOV:
+	case OHMD_LEFT_EYE_FOV:
+	{
+		float[1] out;
+		jfloatArray jout = (*env)->NewFloatArray(1);
+		
+		ohmd_device_getf(device, type, cout);
+		(*env)->SetFloatArrayRegion(env, jout, 0, 1, cout);
+
+		return jout;
+	}
+	case OHMD_RIGHT_EYE_ASPECT_RATIO:
+	case OHMD_LEFT_EYE_ASPECT_RATIO:
+	{
+		float[1] out;
+		jfloatArray jout = (*env)->NewFloatArray(1);
+		
+		ohmd_device_getf(device, type, cout);
+		(*env)->SetFloatArrayRegion(env, jout, 0, 1, cout);
+
+		return jout;
+	}
+
+	case OHMD_EYE_IPD:
+	{
+		float[1] out;
+		jfloatArray jout = (*env)->NewFloatArray(1);
+		
+		ohmd_device_getf(device, type, cout);
+		(*env)->SetFloatArrayRegion(env, jout, 0, 1, cout);
+
+		return jout;
+	}
+
+	case OHMD_PROJECTION_ZFAR:
+	{
+		float[1] out;
+		jfloatArray jout = (*env)->NewFloatArray(1);
+		
+		ohmd_device_getf(device, type, cout);
+		(*env)->SetFloatArrayRegion(env, jout, 0, 1, cout);
+
+		return jout;
+	}
+	case OHMD_PROJECTION_ZNEAR:
+	{
+		float[1] out;
+		jfloatArray jout = (*env)->NewFloatArray(1);
+		
+		ohmd_device_getf(device, type, cout);
+		(*env)->SetFloatArrayRegion(env, jout, 0, 1, cout);
+
+		return jout;
+	}
+
+	case OHMD_ROTATION_QUAT:
+	{
+		float[4] out;
+		jfloatArray jout = (*env)->NewFloatArray(4);
+		
+		ohmd_device_getf(device, type, cout);
+		(*env)->SetFloatArrayRegion(env, jout, 0, 4, cout);
+
+		return jout;
+	}
+	case OHMD_POSITION_VECTOR:
+	{
+		float[4] out;
+		jfloatArray jout = (*env)->NewFloatArray(4);
+		
+		ohmd_device_getf(device, type, cout);
+		(*env)->SetFloatArrayRegion(env, jout, 0, 4, cout);
+
+		return jout;
+	}
+		
+	default:
+		return device->getf(device, type, out);
+	}
+	
+	return 0; //something horrible has gone wrong
 }
 
-JNIEXPORT jint JNICALL Java_OpenHMD_ohmd_device_setf(JNIEnv* env, jobject obj, jlong context, jint type, jfloat* in);
+JNIEXPORT jint JNICALL Java_OpenHMD_ohmd_device_setf(JNIEnv* env, jobject obj, jlong context, jint type, jfloatArray in);
 {
 	ohmd_context* ctx = (ohmd_context*)context;
 
@@ -91,14 +249,15 @@ JNIEXPORT jint JNICALL Java_OpenHMD_ohmd_device_setf(JNIEnv* env, jobject obj, j
 	return ohmd_device_setf(device, type, in);
 }
 
-JNIEXPORT jint* JNICALL Java_OpenHMD_ohmd_device_geti(JNIEnv* env, jobject obj, jlong context, jint type);
+JNIEXPORT jintArray JNICALL Java_OpenHMD_ohmd_device_geti(JNIEnv* env, jobject obj, jlong context, jint type);
 {
+	/*
 	ohmd_context* ctx = (ohmd_context*)context;
 	int* cout;
-	jint* jout;
+	jintArray jout;
 
 	ohmd_device_geti(device, type, cout);
 	jout = (*env)->GetIntArrayElements(env, cout, 0);
 
-	return jout;
+	return jout;*/
 }

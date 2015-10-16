3b059d50215017515c58029ec2c1a184e46677ef
Galadus <koen.mmm@gmail.com>
2015-10-16 15:11:36 +0200

    - added Small test main in java OpenHMD class - fixed signature of java functions - made c bindings compile and work

--- OpenHMD-Java.c
+++ OpenHMD-Java.c
@@ -1,23 +1,28 @@
-#include <jni.h>
 #include "OpenHMD-Java.h"
+
 #include "openhmd.h"
 
-JNIEXPORT jlong JNICALL Java_OpenHMD_ohmd_ctx_create(JNIEnv* env, jobject obj);
+#include <jni.h>
+
+jlong Java_OpenHMD_ohmd_1ctx_1create
+  (JNIEnv *env, jobject obj)
 {
 	//Create the context and pass pointer as long to Java
 	ohmd_context* ctx = ohmd_ctx_create();
-	
+
 	return (long)ctx;
 }
 
-JNIEXPORT void JNICALL Java_ohmd_ctx_destroy(JNIEnv* env, jobject obj, jlong context);
+void Java_OpenHMD_ohmd_1ctx_1destroy
+  (JNIEnv *env, jobject obj, jlong context)
 {
 	//Destroy the context
 	ohmd_context* ctx = (ohmd_context*)context;
 	ohmd_ctx_destroy(ctx);
 }
 
-JNIEXPORT jstring JNICALL Java_OpenHMD_OpenHMD_ohmd_ctx_get_error(JNIEnv* env, jobject obj, jlong context);
+jstring Java_OpenHMD_ohmd_1ctx_1get_1error
+  (JNIEnv *env, jobject obj, jlong context)
 {
 	//Call get_error and return the error code
 	ohmd_context* ctx = (ohmd_context*)context;
@@ -29,109 +34,115 @@ JNIEXPORT jstring JNICALL Java_OpenHMD_OpenHMD_ohmd_ctx_get_error(JNIEnv* env, j
 	return error;
 }
 
-JNIEXPORT void JNICALL Java_OpenHMD_ohmd_ctx_update(JNIEnv* env, jobject obj, jlong context);
+void Java_OpenHMD_ohmd_1ctx_1update
+  (JNIEnv *env, jobject obj, jlong context)
 {
 	ohmd_context* ctx = (ohmd_context*)context;
 	ohmd_ctx_update(ctx);
 }
 
-JNIEXPORT jint JNICALL Java_OpenHMD_ohmd_ctx_probe(JNIEnv* env, jobject obj, jlong context);
+jint Java_OpenHMD_ohmd_1ctx_1probe
+  (JNIEnv *env, jobject obj, jlong context)
 {
 	ohmd_context* ctx = (ohmd_context*)context;
-	
+
 	return ohmd_ctx_probe(ctx);
 }
 
-JNIEXPORT jstring JNICALL Java_OpenHMD_ohmd_list_gets(JNIEnv* env, jobject obj, jlong context, jint index, jint type);
+jstring Java_OpenHMD_ohmd_1list_1gets
+  (JNIEnv *env, jobject obj, jlong context, jint index, jint type)
 {
 	ohmd_context* ctx = (ohmd_context*)context;
 	const char* creturn = ohmd_list_gets(ctx, index, type);
 	jstring jreturn;
 
-	jreturn = (*env)->NewStringUTF(env,jreturn);
+	jreturn = (*env)->NewStringUTF(env,creturn);
 
 	return jreturn;
 }
 
-JNIEXPORT jlong JNICALL Java_OpenHMD_ohmd_list_open_device(JNIEnv* env, jobject obj, jlong context, jint index);
+jlong Java_OpenHMD_ohmd_1list_1open_1device
+  (JNIEnv *env, jobject obj, jlong context, jint index)
 {
 	ohmd_context* ctx = (ohmd_context*)context;
-	ohmd_device* dev = ohmd_list_open_device(ctx, index)
-	
+	ohmd_device* dev = ohmd_list_open_device(ctx, index);
+
 	//returns pointer to device as long
 	return (long)dev;
-	
+
 }
 
-JNIEXPORT jint JNICALL Java_OpenHMD_ohmd_close_device(JNIEnv* env, jobject obj, jlong context);
+jint Java_OpenHMD_ohmd_1close_1device
+  (JNIEnv *env, jobject obj, jlong context)
 {
-	ohmd_context* ctx = (ohmd_context*)context;
-	
+	ohmd_device* dev = (ohmd_device*)context;
+
 	//return int is error code
-	return ohmd_close_device(device);
+	return ohmd_close_device(dev);
 }
 
-JNIEXPORT jfloatArray JNICALL Java_OpenHMD_ohmd_device_getf(JNIEnv* env, jobject obj, jlong context, jint type);
+jfloatArray Java_OpenHMD_ohmd_1device_1getf
+  (JNIEnv *env, jobject obj, jlong device, jint type)
 {
-	ohmd_context* ctx = (ohmd_context*)context;
-	
+	ohmd_device* dev = (ohmd_device*)device;
+
 	switch(type){
-	case OHMD_LEFT_EYE_GL_MODELVIEW_MATRIX: 
+	case OHMD_LEFT_EYE_GL_MODELVIEW_MATRIX:
 	{
-		float[16] cout;
-		jfloatArray jout = (*env)->NewFloatArray(16);
-		
-		ohmd_device_getf(device, type, cout);
+		float cout[16];
+		jfloatArray jout = (*env)->NewFloatArray(env, 16);
+
+		ohmd_device_getf(dev, type, cout);
 		(*env)->SetFloatArrayRegion(env, jout, 0, 16, cout);
 
 		return jout;
 	}
-	case OHMD_RIGHT_EYE_GL_MODELVIEW_MATRIX: 
+	case OHMD_RIGHT_EYE_GL_MODELVIEW_MATRIX:
 	{
-		float[16] out;
-		jfloatArray jout = (*env)->NewFloatArray(16);
-		
-		ohmd_device_getf(device, type, cout);
+		float cout[16];
+		jfloatArray jout = (*env)->NewFloatArray(env, 16);
+
+		ohmd_device_getf(dev, type, cout);
 		(*env)->SetFloatArrayRegion(env, jout, 0, 16, cout);
 
 		return jout;
 	}
 	case OHMD_LEFT_EYE_GL_PROJECTION_MATRIX:
 	{
-		float[16] out;
-		jfloatArray jout = (*env)->NewFloatArray(16);
-		
-		ohmd_device_getf(device, type, cout);
+		float cout[16];
+		jfloatArray jout = (*env)->NewFloatArray(env, 16);
+
+		ohmd_device_getf(dev, type, cout);
 		(*env)->SetFloatArrayRegion(env, jout, 0, 16, cout);
 
 		return jout;
 	}
 	case OHMD_RIGHT_EYE_GL_PROJECTION_MATRIX:
 	{
-		float[16] out;
-		jfloatArray jout = (*env)->NewFloatArray(16);
-		
-		ohmd_device_getf(device, type, cout);
+		float cout[16];
+		jfloatArray jout = (*env)->NewFloatArray(env, 16);
+
+		ohmd_device_getf(dev, type, cout);
 		(*env)->SetFloatArrayRegion(env, jout, 0, 16, cout);
 
 		return jout;
 	}
 	case OHMD_SCREEN_HORIZONTAL_SIZE:
 	{
-		float[1] out;
-		jfloatArray jout = (*env)->NewFloatArray(1);
-		
-		ohmd_device_getf(device, type, cout);
+		float cout[1];
+		jfloatArray jout = (*env)->NewFloatArray(env, 1);
+
+		ohmd_device_getf(dev, type, cout);
 		(*env)->SetFloatArrayRegion(env, jout, 0, 1, cout);
 
 		return jout;
 	}
 	case OHMD_SCREEN_VERTICAL_SIZE:
 	{
-		float[1] out;
-		jfloatArray jout = (*env)->NewFloatArray(1);
-		
-		ohmd_device_getf(device, type, cout);
+		float cout[1];
+		jfloatArray jout = (*env)->NewFloatArray(env, 1);
+
+		ohmd_device_getf(dev, type, cout);
 		(*env)->SetFloatArrayRegion(env, jout, 0, 1, cout);
 
 		return jout;
@@ -139,20 +150,20 @@ JNIEXPORT jfloatArray JNICALL Java_OpenHMD_ohmd_device_getf(JNIEnv* env, jobject
 
 	case OHMD_LENS_HORIZONTAL_SEPARATION:
 	{
-		float[1] out;
-		jfloatArray jout = (*env)->NewFloatArray(1);
-		
-		ohmd_device_getf(device, type, cout);
+		float cout[1];
+		jfloatArray jout = (*env)->NewFloatArray(env, 1);
+
+		ohmd_device_getf(dev, type, cout);
 		(*env)->SetFloatArrayRegion(env, jout, 0, 1, cout);
 
 		return jout;
 	}
 	case OHMD_LENS_VERTICAL_POSITION:
 	{
-		float[1] out;
-		jfloatArray jout = (*env)->NewFloatArray(1);
-		
-		ohmd_device_getf(device, type, cout);
+		float cout[1];
+		jfloatArray jout = (*env)->NewFloatArray(env, 1);
+
+		ohmd_device_getf(dev, type, cout);
 		(*env)->SetFloatArrayRegion(env, jout, 0, 1, cout);
 
 		return jout;
@@ -161,10 +172,10 @@ JNIEXPORT jfloatArray JNICALL Java_OpenHMD_ohmd_device_getf(JNIEnv* env, jobject
 	case OHMD_RIGHT_EYE_FOV:
 	case OHMD_LEFT_EYE_FOV:
 	{
-		float[1] out;
-		jfloatArray jout = (*env)->NewFloatArray(1);
-		
-		ohmd_device_getf(device, type, cout);
+		float cout[1];
+		jfloatArray jout = (*env)->NewFloatArray(env, 1);
+
+		ohmd_device_getf(dev, type, cout);
 		(*env)->SetFloatArrayRegion(env, jout, 0, 1, cout);
 
 		return jout;
@@ -172,10 +183,10 @@ JNIEXPORT jfloatArray JNICALL Java_OpenHMD_ohmd_device_getf(JNIEnv* env, jobject
 	case OHMD_RIGHT_EYE_ASPECT_RATIO:
 	case OHMD_LEFT_EYE_ASPECT_RATIO:
 	{
-		float[1] out;
-		jfloatArray jout = (*env)->NewFloatArray(1);
-		
-		ohmd_device_getf(device, type, cout);
+		float cout[1];
+		jfloatArray jout = (*env)->NewFloatArray(env, 1);
+
+		ohmd_device_getf(dev, type, cout);
 		(*env)->SetFloatArrayRegion(env, jout, 0, 1, cout);
 
 		return jout;
@@ -183,10 +194,10 @@ JNIEXPORT jfloatArray JNICALL Java_OpenHMD_ohmd_device_getf(JNIEnv* env, jobject
 
 	case OHMD_EYE_IPD:
 	{
-		float[1] out;
-		jfloatArray jout = (*env)->NewFloatArray(1);
-		
-		ohmd_device_getf(device, type, cout);
+		float cout[1];
+		jfloatArray jout = (*env)->NewFloatArray(env, 1);
+
+		ohmd_device_getf(dev, type, cout);
 		(*env)->SetFloatArrayRegion(env, jout, 0, 1, cout);
 
 		return jout;
@@ -194,20 +205,20 @@ JNIEXPORT jfloatArray JNICALL Java_OpenHMD_ohmd_device_getf(JNIEnv* env, jobject
 
 	case OHMD_PROJECTION_ZFAR:
 	{
-		float[1] out;
-		jfloatArray jout = (*env)->NewFloatArray(1);
-		
-		ohmd_device_getf(device, type, cout);
+		float cout[1];
+		jfloatArray jout = (*env)->NewFloatArray(env, 1);
+
+		ohmd_device_getf(dev, type, cout);
 		(*env)->SetFloatArrayRegion(env, jout, 0, 1, cout);
 
 		return jout;
 	}
 	case OHMD_PROJECTION_ZNEAR:
 	{
-		float[1] out;
-		jfloatArray jout = (*env)->NewFloatArray(1);
-		
-		ohmd_device_getf(device, type, cout);
+		float cout[1];
+		jfloatArray jout = (*env)->NewFloatArray(env, 1);
+
+		ohmd_device_getf(dev, type, cout);
 		(*env)->SetFloatArrayRegion(env, jout, 0, 1, cout);
 
 		return jout;
@@ -215,41 +226,44 @@ JNIEXPORT jfloatArray JNICALL Java_OpenHMD_ohmd_device_getf(JNIEnv* env, jobject
 
 	case OHMD_ROTATION_QUAT:
 	{
-		float[4] out;
-		jfloatArray jout = (*env)->NewFloatArray(4);
-		
-		ohmd_device_getf(device, type, cout);
+		float cout[4];
+		jfloatArray jout = (*env)->NewFloatArray(env, 4);
+
+		ohmd_device_getf(dev, type, cout);
 		(*env)->SetFloatArrayRegion(env, jout, 0, 4, cout);
 
 		return jout;
 	}
 	case OHMD_POSITION_VECTOR:
 	{
-		float[4] out;
-		jfloatArray jout = (*env)->NewFloatArray(4);
-		
-		ohmd_device_getf(device, type, cout);
+		float cout[3];
+		jfloatArray jout = (*env)->NewFloatArray(env, 3);
+
+		ohmd_device_getf(dev, type, cout);
 		(*env)->SetFloatArrayRegion(env, jout, 0, 4, cout);
 
 		return jout;
 	}
-		
+
 	default:
-		return device->getf(device, type, out);
+	    return 0;
+		//return dev->getf(dev, type, out);
 	}
-	
+
 	return 0; //something horrible has gone wrong
 }
 
-JNIEXPORT jint JNICALL Java_OpenHMD_ohmd_device_setf(JNIEnv* env, jobject obj, jlong context, jint type, jfloatArray in);
+jint Java_OpenHMD_ohmd_1device_1setf
+  (JNIEnv *env, jobject obj, jlong context, jint type, jfloatArray in)
 {
 	ohmd_context* ctx = (ohmd_context*)context;
 
 	//return int is error code
-	return ohmd_device_setf(device, type, in);
+	//return ohmd_device_setf(device, type, in);
 }
 
-JNIEXPORT jintArray JNICALL Java_OpenHMD_ohmd_device_geti(JNIEnv* env, jobject obj, jlong context, jint type);
+jintArray Java_OpenHMD_ohmd_1device_1geti
+  (JNIEnv *env, jobject obj, jlong device, jint type)
 {
 	/*
 	ohmd_context* ctx = (ohmd_context*)context;
--- OpenHMD.java
+++ OpenHMD.java
@@ -2,25 +2,38 @@ public class OpenHMD
 {
 	static 
 	{
-		System.load("/home/koen/Code/OpenHMD-Java/libOpenHMD.so");
+		System.load("OpenHMD");
 	}
 	
-	
-	
-//	private native void ohmd_ctx_create();
-	private native void 	ohmd_ctx_destroy();
-	private native String 	ohmd_ctx_get_error();
-	private native void 	ohmd_ctx_update();
-	private native int 		ohmd_ctx_probe();
-	private native String 	ohmd_list_gets();
-//	private native void ohmd_list_open_device();
-	private native int 		ohmd_close_device();
-	private native float 	ohmd_device_getf();
-	private native int 		ohmd_device_setf();
-	private native int 		ohmd_device_geti();
+	public static native long 		ohmd_ctx_create();
+	public static native void 		ohmd_ctx_destroy(long ctx);
+	public static native String 	ohmd_ctx_get_error(long ctx);
+	public static native void 		ohmd_ctx_update(long ctx);
+	public static native int 		ohmd_ctx_probe(long ctx);
+	public static native String 	ohmd_list_gets(long dev, int index, int ohmd_string_value); 
+	public static native long 		ohmd_list_open_device(long ctx, int index);
+	public static native int 		ohmd_close_device(long ctx);
+	public static native float[] 	ohmd_device_getf(long dev, int ohmd_float_value);
+	public static native int 		ohmd_device_setf(long dev, int ohmd_float_value, float[] values);
+	public static native int[]		ohmd_device_geti(long dev, int ohmd_int_value);
 
 	
 	public static void main(String[] args)
 	{
+		long ctx = ohmd_ctx_create();
+		System.out.println("ctx: " + ctx);
+		
+		int numDevices = ohmd_ctx_probe(ctx);
+		System.out.println("numDevices (probe): " + numDevices);
+		
+		long dev = ohmd_list_open_device(ctx, 0);
+		System.out.println("dev: " + dev);
+	
+		ohmd_ctx_update(ctx);
+		
+		
+		float[] rot = ohmd_device_getf(dev, 1);
+		for (float f : rot)
+			System.out.println(f + ", ");
 	}
 }
